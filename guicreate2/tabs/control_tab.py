c5536806ee8b7d8a82670ae1c96777580d860259
ed <edoardo.sorelli@gmail.com>
2024-09-28 19:01:57 +0200

    control linear and angular speed directly

--- guicreate2/tabs/control_tab.py
+++ guicreate2/tabs/control_tab.py
@@ -8,6 +8,10 @@ class ControlTab:
 
     def __init__(self, bot: Create2):
 
+        self.bot_width_mm_ = 230
+        self.max_lin_speed_mm_ = 200
+        self.max_ang_speed_rad_ = 90 * 3.141592 / 180
+
         self.bot = bot
         self.control = [0, 0]
         self.control_old = [0, 0]
@@ -22,7 +26,7 @@ class ControlTab:
                             on_move=lambda e: self._update_control_left(e.y),
                             on_end=lambda _: self._update_control_left(0),
                             lockY=True)
-                self.left_control = ui.label('0 mm/s')
+                self.left_control = ui.label('0 deg/s')
             with ui.card().classes('items-center col-span-1'):
                 ui.button('start', on_click=lambda _: self._start_robot())
                 self.mode_dict = {0: 'stop', 1:'passive', 2:'safe', 3:'full'}
@@ -42,17 +46,20 @@ class ControlTab:
         ui.notify('start')
 
     def _update_control_left(self, x):
-        self.left_control.set_text(f"{x * 100:.0f} mm/s")
-        self.control[0] = int(x * 300)
+        speed = x * self.max_ang_speed_rad_
+        self.left_control.set_text(f"{speed * 180 / 3.141592:.0f} deg/s")
+        self.control[0] = int(speed)
 
     def _update_control_right(self, x):
-        self.right_control.set_text(f"{x * 100:.0f} mm/s")
-        self.control[1] = int(x * 300)
+        speed = x * self.max_lin_speed_mm_
+        self.right_control.set_text(f"{speed:.0f} mm/s")
+        self.control[1] = int(speed)
 
     def timer_callback(self):
         if self.control != self.control_old:
             self.control_old = copy.deepcopy(self.control)
-            self.bot.drive_direct(self.control[1], self.control[0])
+            right_wheel_speed, left_wheel_speed = self.wheels_speed(self.control[1], self.control[0])
+            self.bot.drive_direct(right_wheel_speed, left_wheel_speed)
 
     def _update_mode(self, value):
         match self.mode_dict[value]:
@@ -68,3 +75,8 @@ class ControlTab:
             case "full":
                 self.bot.full()
                 ui.notify('full')
+
+    def wheels_speed(self, linear_speed_mm, angular_speed):
+        right_wheel = linear_speed_mm + (angular_speed * self.bot_width_mm_)
+        left_wheel  = linear_speed_mm - (angular_speed * self.bot_width_mm_)
+        return right_wheel, left_wheel
